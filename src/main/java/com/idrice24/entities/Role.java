01f729d06aae64c734e3077a9ed900fb2e1ac99b
idrice24 <idrice.tsafouet@yahoo.com>
2020-04-22 03:20:35 +0100

    Add files via upload

---
+++ src/main/java/com/idrice24/entities/Role.java
@@ -0,0 +1,23 @@
+package com.idrice24.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+    @Column(name = "role")
+    private String role;
+}
