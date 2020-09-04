d2b193aa8769aac9c30bf5021c4d5da4965dfdc9
Ghislain <Gzeleu@gmx.de>
2020-09-04 10:52:39 +0200

    Update Java version and add logger dependecies

--- src/main/java/com/idrice24/controllers/LoginController.java
+++ src/main/java/com/idrice24/controllers/LoginController.java
@@ -2,15 +2,14 @@ package com.idrice24.controllers;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.idrice24.entities.User;
@@ -19,95 +18,28 @@ import com.idrice24.services.UserService;
 @Controller
 public class LoginController {
 
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = { "/" }, method = RequestMethod.GET)
+    @GetMapping("/")
     public ModelAndView begin() {
-	  ModelAndView modelAndView = new ModelAndView();
-	  modelAndView.setViewName("login");
-	  return modelAndView;
+
+	return login();
     }
 
-    @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
+    @GetMapping("/login")
     public ModelAndView login() {
-	  ModelAndView modelAndView = new ModelAndView();
-	  modelAndView.setViewName("login");
-	  return modelAndView;
-    }
 
-    @GetMapping(value = { "/index" })
-    // @RequestMapping(value = { "/index" }, method = RequestMethod.GET)
-    public ModelAndView myindex() {
-   	ModelAndView modelAndView = new ModelAndView();
-	  modelAndView.setViewName("index");
-	  return modelAndView;
-    }
-  
-    @RequestMapping(value = { "/files/studentboard" }, method = RequestMethod.GET)
-    public ModelAndView mystudentboard() {
-	  ModelAndView modelAndView = new ModelAndView();
-	  modelAndView.setViewName("files/studentboard");
-	  return modelAndView;
-    }
-
-    @RequestMapping(value = { "/files/helpboard" }, method = RequestMethod.GET)
-    public ModelAndView myhelpboard() {
-	  ModelAndView modelAndView = new ModelAndView();
-	  modelAndView.setViewName("files/helpboard");
-	   return modelAndView;
-    }
-
-
-    @RequestMapping(value = { "/files/publicboard" }, method = RequestMethod.GET)
-    public ModelAndView mypublicboard() {
-	  ModelAndView modelAndView = new ModelAndView();
-	  modelAndView.setViewName("files/publicboard");
-	  return modelAndView;
-    }
-
-
-    @RequestMapping(value = { "/files/parentsboard" }, method = RequestMethod.GET)
-    public ModelAndView myparentsboard() {
 	ModelAndView modelAndView = new ModelAndView();
-	modelAndView.setViewName("files/parentsboard");
-	return modelAndView;
-    }
-
-
-
-    @RequestMapping(value = { "/files/schoolboard" }, method = RequestMethod.GET)
-    public ModelAndView myschoolboard() {
-	ModelAndView modelAndView = new ModelAndView();
-	modelAndView.setViewName("files/schoolboard");
-	return modelAndView;
-    }
-
-
-    @RequestMapping(value = { "/files/settingboard" }, method = RequestMethod.GET)
-    public ModelAndView mysettingboard() {
-	ModelAndView modelAndView = new ModelAndView();
-	modelAndView.setViewName("files/settingboard");
-	return modelAndView;
-    }
-
-    @GetMapping(value = "/files/teacherboard")
-    public ModelAndView myteacherboard() {
-	ModelAndView modelAndView = new ModelAndView();
-	modelAndView.setViewName("files/teacherboard");
-	return modelAndView;
-    }
-
-
-    @RequestMapping(value = { "/files/tutorialboard" }, method = RequestMethod.GET)
-    public ModelAndView mytutorialboard() {
-	ModelAndView modelAndView = new ModelAndView();
-	modelAndView.setViewName("files/tutorialboard");
+	modelAndView.setViewName("login");
 	return modelAndView;
     }
 
     @GetMapping(value = "/registration")
     public ModelAndView registration() {
+
+	logger.info("registration CALLED");
 
 	ModelAndView modelAndView = new ModelAndView();
 
@@ -116,14 +48,23 @@ public class LoginController {
 
 	// Set up model
 	modelAndView.addObject("user", user);
+
 	// Set up View
 	modelAndView.setViewName("registration");
+
+	// Return the model and his view!!
 	return modelAndView;
     }
 
     @PostMapping(value = "/registration")
     public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
 	ModelAndView modelAndView = new ModelAndView();
+
+	logger.info(user.getEmail());
+	logger.info("" + bindingResult.getErrorCount());
+	for (ObjectError er : bindingResult.getAllErrors()) {
+	    logger.info(er.getDefaultMessage());
+	}
 	User userExists = userService.findUserByEmail(user.getEmail());
 	if (userExists != null) {
 	    bindingResult.rejectValue("email", "error.user",
@@ -138,18 +79,6 @@ public class LoginController {
 	    modelAndView.setViewName("registration");
 
 	}
-	return modelAndView;
-    }
-
-    @GetMapping(value = "/admin/home")
-    public ModelAndView home() {
-	ModelAndView modelAndView = new ModelAndView();
-	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	User user = userService.findUserByEmail(auth.getName());
-	modelAndView.addObject("userName",
-		"Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-	modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
-	modelAndView.setViewName("admin/home");
 	return modelAndView;
     }
 
--- src/main/java/com/idrice24/services/UserService.java
+++ src/main/java/com/idrice24/services/UserService.java
@@ -1,15 +1,12 @@
 package com.idrice24.services;
 
-import com.idrice24.entities.Role;
-import com.idrice24.entities.User;
-import com.idrice24.repositories.RoleRepository;
-import com.idrice24.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import com.idrice24.entities.User;
+import com.idrice24.repositories.RoleRepository;
+import com.idrice24.repositories.UserRepository;
 
 @Service("userService")
 public class UserService {
@@ -19,24 +16,20 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository,
-                       RoleRepository roleRepository,
-                       BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    public UserService(UserRepository userRepository, RoleRepository roleRepository,
+	    BCryptPasswordEncoder bCryptPasswordEncoder) {
+	this.userRepository = userRepository;
+	this.roleRepository = roleRepository;
+	this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     public User findUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+	return userRepository.findByEmail(email);
     }
 
     public User saveUser(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setActive(1);
-        Role userRole = roleRepository.findByRole("ADMIN");
-        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
-        return userRepository.save(user);
+
+	return userRepository.save(user);
     }
 
 }
