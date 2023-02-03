a7df1fc88617d4b1a2f955ead52a9b855ffb7e84
idrice24 <idrice.tsafouet@yahoo.com>
2023-02-03 13:37:21 +0100

    update files

---
+++ src/test/java/org/idrice24/Idrice24ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.idrice24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idrice24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
