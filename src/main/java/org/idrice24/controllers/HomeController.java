f3a24aecfab1650f4829c1420b60334429370874
idrice24 <idrice.tsafouet@yahoo.com>
2023-02-03 18:57:49 +0100

    Update HomeController.java

--- src/main/java/org/idrice24/controllers/HomeController.java
+++ src/main/java/org/idrice24/controllers/HomeController.java
@@ -1,9 +1,7 @@
 package org.idrice24.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.stereotype.Controller;
 
