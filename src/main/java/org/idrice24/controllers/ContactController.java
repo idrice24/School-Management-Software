fd03039fc137a16cf2a13e8c389917c1bd3a2c5a
idrice24 <idrice.tsafouet@yahoo.com>
2023-02-18 12:32:19 +0100

    update contact file #83

--- src/main/java/org/idrice24/controllers/ContactController.java
+++ src/main/java/org/idrice24/controllers/ContactController.java
@@ -4,13 +4,12 @@ package org.idrice24.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 
 import org.idrice24.entities.Contact;
 import org.idrice24.services.ContactService;
@@ -20,30 +19,6 @@ import org.idrice24.services.ContactService;
 public class ContactController{
 
 	private ContactService contactService;
-
-	public void setContactService(ContactService contactService){
-		this.contactService = contactService;
-	}
-
-	@GetMapping("contactform")
-	public String showForm(Contact contact, BindingResult result){
-		if(result.hasErrors()){
-			return "contact";
-		}
-		return "contact";
-	}
-
-	@PostMapping("contactform/send")
-	public String sendContact(@Valid Contact contact, Model model, BindingResult result){
-		if(result.hasErrors()){
-			return "contact";
-		}
-		model.addAttribute("noErrors", true);
-		model.addAttribute("contact", contact);
-		String subject = contact.getName() + " " + contact.getEmail() + " send you a message";
-		contactService.sentContact(subject, contact.getMessage());
-		return "contact";
-	}
 
 
 }
