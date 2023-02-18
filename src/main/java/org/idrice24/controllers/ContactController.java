3b47dbed256d401ccd92294d0113097478f90b31
idrice24 <idrice.tsafouet@yahoo.com>
2023-02-18 16:59:20 +0100

    Update ContactController.java

--- src/main/java/org/idrice24/controllers/ContactController.java
+++ src/main/java/org/idrice24/controllers/ContactController.java
@@ -1,24 +1,15 @@
 package org.idrice24.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
-
-import org.idrice24.entities.Contact;
-import org.idrice24.services.ContactService;
 
 @Controller
 @RequestMapping("/contact/")
 public class ContactController{
 
-	private ContactService contactService;
 
 
 }
